0115565b450e50ba55339ac4ae7e650f61a57bbb
Hju Kneyck Flores <hju.halcyondigital@gmail.com>
2023-02-08 12:45:42 +0800

    Update cart page layout

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,7 @@
 import {
+  ActionIcon,
   AppShell,
+  Button,
   Header,
   Box,
   MediaQuery,
@@ -9,8 +11,16 @@ import {
   Navbar,
   Image,
   Flex,
+  Paper,
+  Container,
+  ScrollArea,
+  Grid,
+  Divider,
+  NavLink,
 } from '@mantine/core';
 import { useListState } from '@mantine/hooks';
+import { IconShoppingCart, IconTrash, IconUser } from '@tabler/icons';
+import Link from 'next/link';
 import React from 'react';
 
 type Item = {
@@ -41,33 +51,91 @@ const mockdata = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     checked: false,
   },
+  {
+    id: 3,
+    name: 'Nike Air Force 1',
+    price: 100,
+    image:
+      'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/73eb7950-edf2-4dbf-96c3-9b684fe974a9/flex-experience-run-11-next-nature-road-running-shoes-rB6h46.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    checked: false,
+  },
+  {
+    id: 4,
+    name: 'Nike Air Force 1',
+    price: 100,
+    image:
+      'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/73eb7950-edf2-4dbf-96c3-9b684fe974a9/flex-experience-run-11-next-nature-road-running-shoes-rB6h46.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    checked: false,
+  },
+  {
+    id: 5,
+    name: 'Nike Air Force 1',
+    price: 100,
+    image:
+      'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/73eb7950-edf2-4dbf-96c3-9b684fe974a9/flex-experience-run-11-next-nature-road-running-shoes-rB6h46.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    checked: false,
+  },
+  {
+    id: 6,
+    name: 'Nike Air Force 1',
+    price: 100,
+    image:
+      'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/73eb7950-edf2-4dbf-96c3-9b684fe974a9/flex-experience-run-11-next-nature-road-running-shoes-rB6h46.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    checked: false,
+  },
 ];
 
 interface SyntheticEvent<T> {
   currentTarget: EventTarget & T;
 }
 
+const DELIVERY_FEE = 50;
+
 export default function Cart() {
   const [opened, setOpened] = React.useState(false);
   const [selectedItems, setSelectedItems] = React.useState<Item[]>([]);
   const [values, handlers] = useListState(mockdata);
+  const [hasSubtotal, setHasSubtotal] = React.useState(false);
 
   const allChecked = values.every((value) => value.checked);
+
   const indeterminate = values.some((value) => value.checked) && !allChecked;
+
+  const handleSelectAll = (event: SyntheticEvent<HTMLInputElement>) => {
+    handlers.setState((current) =>
+      current.map((value) => ({ ...value, checked: !allChecked }))
+    );
+
+    setSelectedItems(event.currentTarget.checked ? [...values] : []);
+  };
 
   const handleSelect = (
     event: SyntheticEvent<HTMLInputElement>,
-    item: Item
+    item: Item,
+    index: number
   ) => {
-    handlers.setItemProp(item.id, 'checked', event.currentTarget.checked);
+    handlers.setItemProp(index, 'checked', event.currentTarget.checked);
 
     setSelectedItems((items) => {
-      if (items.includes(item)) {
+      if (items.filter((i) => i.id === item.id).length > 0) {
+        console.log('remove');
         return items.filter((i) => i.id !== item.id);
       }
 
       return [...items, item];
     });
+  };
+
+  const computeSubtotal = () => {
+    return selectedItems.reduce((acc, item) => acc + item.price, 0);
+  };
+
+  const computeTotal = () => {
+    return computeSubtotal() + DELIVERY_FEE;
   };
 
   const cartItems = values.map((item, index) => (
@@ -82,13 +150,7 @@ export default function Cart() {
         <Flex align="center" justify="center" gap="sm">
           <Checkbox
             checked={item.checked}
-            onChange={(event) =>
-              handlers.setItemProp(
-                index,
-                'checked',
-                event.currentTarget.checked
-              )
-            }
+            onChange={(event) => handleSelect(event, item, index)}
           />
           <Box
             sx={{
@@ -107,7 +169,9 @@ export default function Cart() {
           </Box>
           <Text>{item.name}</Text>
         </Flex>
-        <Text>${item.price}</Text>
+        <Text mx="xl" weight={700}>
+          ₱{item.price}
+        </Text>
       </Box>
     </Box>
   ));
@@ -143,23 +207,91 @@ export default function Cart() {
           hidden={!opened}
           width={{ sm: 200, lg: 300 }}
         >
-          <></>
+          <Link href="/" style={{ textDecoration: 'none' }}>
+            <NavLink label="Account" icon={<IconUser size={18} />} />
+          </Link>
+          <NavLink label="Cart" icon={<IconShoppingCart size={18} />} />
         </Navbar>
       }
     >
-      <h1>Cart</h1>
-      <Checkbox
-        checked={allChecked}
-        indeterminate={indeterminate}
-        label="Select all"
-        transitionDuration={0}
-        onChange={() =>
-          handlers.setState((current) =>
-            current.map((value) => ({ ...value, checked: !allChecked }))
-          )
-        }
-      />
-      {cartItems}
+      <Flex wrap="wrap" w="100%" gap="sm">
+        <Paper
+          shadow="xs"
+          p="md"
+          withBorder
+          sx={{
+            flexGrow: 1,
+          }}
+        >
+          <Flex align="center" justify="space-between" mb="sm">
+            <Checkbox
+              checked={allChecked}
+              indeterminate={indeterminate}
+              label="Select all"
+              onChange={(e) => handleSelectAll(e)}
+            />
+            <ActionIcon color="dark" size="sm" variant="transparent">
+              <IconTrash />
+            </ActionIcon>
+          </Flex>
+
+          <ScrollArea style={{ height: 250 }}>{cartItems}</ScrollArea>
+        </Paper>
+        <Paper
+          shadow="xs"
+          p="md"
+          withBorder
+          sx={{
+            flexGrow: 1,
+          }}
+        >
+          <Flex direction="column" justify="space-between" h="100%">
+            <Text>Selected items: {selectedItems.length}</Text>
+
+            <Divider />
+
+            <Grid mt="sm">
+              <Grid.Col span={6}>
+                <Text>Subtotal</Text>
+                <Text>Shipping</Text>
+                <Text>Tax</Text>
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <Text align="right">₱{computeSubtotal()}</Text>
+                <Text align="right">₱{DELIVERY_FEE}</Text>
+                <Text align="right">₱0</Text>
+              </Grid.Col>
+            </Grid>
+
+            <Divider />
+
+            <Grid mt="sm">
+              <Grid.Col span={6}>
+                <Text>Total</Text>
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <Flex align="center" justify="flex-end" gap="sm">
+                  <Text fz="xs" c="dimmed">
+                    PHP
+                  </Text>
+                  <Text fw={700} size="xl">
+                    ₱{computeTotal()}
+                  </Text>
+                </Flex>
+              </Grid.Col>
+            </Grid>
+
+            <Button
+              mt="md"
+              size="lg"
+              fullWidth
+              disabled={selectedItems.length === 0}
+            >
+              Checkout
+            </Button>
+          </Flex>
+        </Paper>
+      </Flex>
     </AppShell>
   );
 }
